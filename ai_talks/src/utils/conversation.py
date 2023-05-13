d81e342580c72b61b5c9c0e8a9d4fd7ff768f6e0
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-14 05:45:41 +0800

    Update conversation.py

--- ai_talks/src/utils/conversation.py
+++ ai_talks/src/utils/conversation.py
@@ -33,7 +33,7 @@ def show_query_result() -> None:
     qd=get_queries_data('2022-05-13','2023-05-13',PAYMENT_QUERIES_QUERY)
     st.text_area(label="生成的SQL代码", value=qd.shape, key="query_result")
 #     st.bar_chart(data=qd,x="QUERY_TYPE",y="TOTAL_ELAPSED_TIME")
-#     st.table(qd)
+    st.table(qd)
     
 
 def clear_chat() -> None:
