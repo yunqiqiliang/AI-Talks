e1726b2b892840c23ea479723a3e25515b24ba39
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-14 01:10:32 +0800

    Update snowflake_connector.py

--- ai_talks/src/utils/snowflake_connector.py
+++ ai_talks/src/utils/snowflake_connector.py
@@ -61,8 +61,8 @@ snowflake_connector = get_connector(
 )
 
 cur = snowflake_connector.cursor()
-cur.execute(f"use warehouse {st.secrets.sf_usage_app.warehouse};")
-# cur.execute(f"use role {st.secrets.sf_usage_app.role};")
+cur.execute(f"use warehouse {st.secrets.snowflake_warehouse_app.warehouse};")
+cur.execute(f"use role {st.secrets.snowflake_warehouse_app.role};")
 
 
 @st.experimental_memo(ttl=TIME_TO_LIVE)
