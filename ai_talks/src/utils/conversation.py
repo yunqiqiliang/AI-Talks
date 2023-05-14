fcc61304adc8336397fa5089964635023beeadec
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-14 11:10:47 +0800

    Update conversation.py

--- ai_talks/src/utils/conversation.py
+++ ai_talks/src/utils/conversation.py
@@ -9,7 +9,7 @@ from .stt import show_voice_input
 from .tts import show_audio_player
 
 from .snowflake_connector import get_queries_data
-from utils import preparedprompts
+from preparedprompts import prompts_schema,prompts_ord,prompts_sql_standard
 
 import logging
 import re
