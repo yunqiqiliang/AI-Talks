d63b7c5f7da7179883b184d7a4115ab9a876d697
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-14 12:13:25 +0800

    Update footer.py

--- ai_talks/src/utils/footer.py
+++ ai_talks/src/utils/footer.py
@@ -11,12 +11,12 @@ def show_info(icon: Path) -> None:
     st.markdown(f"<div style='text-align: justify;'>{st.session_state.locale.responsibility_denial}</div>",
                 unsafe_allow_html=True)
     st.divider()
-    st.markdown(f"""
-        ### :page_with_curl: {st.session_state.locale.footer_title}
-        - {render_svg(icon)} [{st.session_state.locale.footer_chat}](https://t.me/talks_ai)
-        - {render_svg(icon)} [{st.session_state.locale.footer_channel}](https://t.me/talks_aii)
-    """, unsafe_allow_html=True)
-    st.divider()
+#     st.markdown(f"""
+#         ### :page_with_curl: {st.session_state.locale.footer_title}
+#         - {render_svg(icon)} [{st.session_state.locale.footer_chat}](https://t.me/talks_ai)
+#         - {render_svg(icon)} [{st.session_state.locale.footer_channel}](https://t.me/talks_aii)
+#     """, unsafe_allow_html=True)
+#     st.divider()
 #     st.markdown(f"project [repo on github]({REPO_URL}) waiting for your :star: | [report]({BUG_REPORT_URL}) a bug")
 
 
