b2ae48cc8e9a7cd168718633eef58ce48dcbe187
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-15 07:39:49 +0800

    Update snowflake_connector.py

--- ai_talks/src/utils/snowflake_connector.py
+++ ai_talks/src/utils/snowflake_connector.py
@@ -66,12 +66,27 @@ cur.execute(f"use role {st.secrets.sf_usage_app.role};")
 
 
 @st.experimental_memo(ttl=TIME_TO_LIVE)
-def sql_to_dataframe(sql_query: str) -> pd.DataFrame:
-    data = pd.read_sql(
-        sql_query,
-        snowflake_connector,
-    )
-    return data
+def sql_to_dataframe(sql_query: str) -> pd.DataFrame, error_code:str, error_reason:str,:
+    error_code=""
+    error_reason=""
+    data = pd.DataFrame()
+    try:
+        # 执行 SQL 查询
+        data = pd.read_sql(
+            sql_query,
+            snowflake_connector,
+        )
+        # 如果查询成功，则对 DataFrame 进行进一步处理
+        return data, error_code,error_reason 
+    except Exception as e:
+        # 如果查询失败，则打印错误代码和错误原因，并进行相应处理
+        print(f"Error code: {e.args[0]}")
+        print(f"Error reason: {e.args[1]}")
+        error_code=e.args[0]
+        error_reason=e.args[1]
+        return data, error_code,error_reason 
+    
+    
 
 
 @st.experimental_memo(ttl=TIME_TO_LIVE)
