33c513687fa665dd1be1c5a9e0aa5503b937cae9
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-15 15:09:57 +0800

    Update lakehouse_connector.py

--- ai_talks/src/utils/lakehouse_connector.py
+++ ai_talks/src/utils/lakehouse_connector.py
@@ -5,5 +5,21 @@ lakehouse_conn = st.experimental_connection(
   type="sql",
   url="clickzetta://robert:xxxxxx@49d58da9.api.clickzetta.com/robert?schema=private&virtualcluster=default"
 )
-
-provs = lakehouse_conn.query("select distinct province from robert.private.geo_cn")
+TIME_TO_LIVE = 60 * 60 * 6  # 6 hours caching
+@st.experimental_memo(ttl=TIME_TO_LIVE)
+def get_queries_data(sql_query: str) -> (pd.DataFrame, str, str):
+    data = pd.DataFrame()
+    error_code = ""
+    error_reason = ""
+    try:
+        # 执行 SQL 查询
+        data = lakehouse_conn.query(sql_query)
+        # 如果查询成功，则对 DataFrame 进行进一步处理
+        return data, error_code, error_reason
+    except Exception as e:
+        # 如果查询失败，则打印错误代码和错误原因，并进行相应处理
+#         print(f"Error code: {e.args[0]}")
+#         print(f"Error reason: {e.args[1]}")
+#         error_code = e.args[0]
+#         error_reason = e.args[1]
+        return data, error_code, error_reason
